8f80163c815812e1c0b1a378cfbfe84ec23563be
Frédéric Jolliton <git@frederic.jolliton.com>
2018-10-05 12:48:31 +0000

    emacs: projectile: reject more directories

--- emacs/.emacs.d/projectile.el
+++ emacs/.emacs.d/projectile.el
@@ -4,4 +4,4 @@
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 (setq projectile-globally-ignored-file-suffixes '("pyc" "whl" "otf" "ttf" "woff" "woff2" "eot"))
-(setq projectile-globally-ignored-directories '(".git" ".pyre" "*node_modules"))
+(setq projectile-globally-ignored-directories '(".git" ".pyre" "*node_modules" "lib/bs" "out"))

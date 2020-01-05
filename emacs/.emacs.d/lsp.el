c427d03a5aae19ca41035e9935667d31d07211cc
Frédéric Jolliton <git@frederic.jolliton.com>
2020-01-05 16:08:35 +0000

    emacs: lsp: use clangd-10 for C

--- emacs/.emacs.d/lsp.el
+++ emacs/.emacs.d/lsp.el
@@ -1,4 +1,10 @@
+(setq lsp-clients-clangd-executable "clangd-10")
+(setq lsp-clients-clangd-args '("--background-index"))
+(setq lsp-enable-on-type-formatting nil)
+
 (add-hook 'js-mode-hook 'lsp-mode)
+
+(add-hook 'c-mode-hook 'lsp)
 
 ;;; Show messages in popup, not in mini-buffer
 (add-hook 'lsp-mode-hook 'lsp-ui-mode)

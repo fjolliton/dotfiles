66d5dd94a07fd7e2101857b8cdd4576a6e1be46a
Frédéric Jolliton <git@frederic.jolliton.com>
2020-12-28 08:36:09 +0000

    emacs: add style for evil + some tweaks

--- emacs/.emacs.d/elegant.el
+++ emacs/.emacs.d/elegant.el
@@ -51,18 +51,44 @@ abused anyway)."
         (modified (and buffer-file-name (buffer-modified-p))))
     (cond (modified  "**") (read-only "RO") (t "RW"))))
 
+(defface evil-face-state nil "Current Evil State" :group 'evil)
+
+(defface evil-face-state-normal nil "Evil State Normal" :group 'evil)
+
+(defface evil-face-state-insert nil "Evil State Insert" :group 'evil)
+
+(defface evil-face-state-visual nil "Evil State Visual" :group 'evil)
+
 (setq-default mode-line-format
   '((:eval
      (mode-line-render
       (format-mode-line
        (let* ((status (nano-modeline-status))
               (prefix (cond ((string= status "RO")
-	 		    (propertize " RO " 'face 'nano-face-header-popout))
+                             (propertize " RO " 'face 'nano-face-header-popout))
                             ((string= status "**")
-	 		    (propertize " ** " 'face 'nano-face-header-critical))
+                             (propertize " ** " 'face 'nano-face-header-critical))
                             ((string= status "RW")
-	 		    (propertize " RW " 'face 'nano-face-header-faded))
-                            (t (propertize status 'face 'nano-face-header-popout)))))
+                             (propertize " RW " 'face 'nano-face-header-faded))
+                            (t
+                             (propertize status 'face 'nano-face-header-popout))))
+              (evil-info (if (boundp 'evil-state)
+                             (propertize
+                              (concat
+                               " "
+                               (if evil-state
+                                   (upcase (subseq (symbol-name evil-state) 0 1))
+                                 "·")
+                               " ")
+                              'face (cond
+                                     ((eq evil-state 'normal)
+                                      'evil-face-state-normal)
+                                     ((eq evil-state 'insert)
+                                      'evil-face-state-insert)
+                                     ((eq evil-state 'visual)
+                                      'evil-face-state-visual)
+                                     (t
+                                      'evil-face-state))))))
          (list
           ;(propertize "☰"
           ;            ;'face `(:inherit mode-line-buffer-id)
@@ -70,22 +96,26 @@ abused anyway)."
           ;            'mouse-face 'mode-line-highlight
           ;            'local-map   mode-line-major-mode-keymap)
           prefix
+          (or evil-info "")
           " %b ")))
       (format-mode-line
        (list
         (propertize "(%m)" 'face 'nano-face-header-default)
         " "
-        (propertize "%4l:%c" 'face `(:inherit face-faded))))))))
+        (propertize "%4l:%02c" 'face `(:inherit face-faded))))))))
 
 (setq-default header-line-format mode-line-format)
 (setq-default mode-line-format '(""))
 
+(defvar tuxee-frame-padding
+  (if (string= (system-name) "mau.intra.tuxee.net") 17 24))
+
 (setq default-frame-alist
-      '((vertical-scroll-bars . nil)
-        (internal-border-width . 24)))
+      `((vertical-scroll-bars . nil)
+        (internal-border-width . ,tuxee-frame-padding)))
 
 (set-frame-parameter (selected-frame)
-                     'internal-border-width 24)
+                     'internal-border-width tuxee-frame-padding)
 
 (setq window-divider-default-right-width 8)
 (setq window-divider-default-places 'right-only)

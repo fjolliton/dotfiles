c6bcd4242b3030348f415f055b53cbc0883b2377
Frédéric Jolliton <github@frederic.jolliton.com>
2021-04-05 08:17:38 +0000

    emacs: flycheck: do not truncate line by default

--- emacs/.emacs.d/flycheck.el
+++ emacs/.emacs.d/flycheck.el
@@ -3,9 +3,14 @@
 ;;; (no comment)
 ;;; Code:
 
+(defun tuxee-disable-truncate-lines ()
+  "Enable truncated lines."
+  (toggle-truncate-lines 0))
+
 (use-package flycheck
   :ensure t
   :hook
+  (flycheck-error-list-mode . tuxee-disable-truncate-lines)
   (prog-mode . global-flycheck-mode))
 
 ;;; flycheck.el ends here

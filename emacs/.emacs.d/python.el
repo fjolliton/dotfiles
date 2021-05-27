83e72e36c808065f1d639c077ce7be1c29fa395f
Frédéric Jolliton <github@frederic.jolliton.com>
2021-05-27 07:10:44 +0000

    emacs: also enable LSP for Python files

--- emacs/.emacs.d/python.el
+++ emacs/.emacs.d/python.el
@@ -6,9 +6,14 @@
 ;;; FIXME: Too specific
 (add-to-list 'exec-path "/home/fred/py3/bin")
 
+(defun tuxee-flycheck-py-setup ()
+  (flycheck-add-next-checker 'lsp 'python-mypy))
+
 (use-package blacken
   :ensure t
   :hook
+  (python-mode . tuxee-flycheck-py-setup)
+  (python-mode . lsp)
   (python-mode . blacken-mode))
 
 ;;; python.el ends here

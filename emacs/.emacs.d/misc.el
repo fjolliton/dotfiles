30319aed9f3da558f0b756739dc4fc7587f11df1
Frédéric Jolliton <github@frederic.jolliton.com>
2021-05-27 07:07:37 +0000

    emacs: multiple-cursors: simpler key bindings

--- emacs/.emacs.d/misc.el
+++ emacs/.emacs.d/misc.el
@@ -60,10 +60,8 @@
 (use-package multiple-cursors
   :ensure t
   :bind
-  ("C-S-c C-S-c" . 'mc/edit-lines)
   ("C->" . 'mc/mark-next-like-this)
-  ("C-<" . 'mc/mark-previous-like-this)
-  ("C-c C-<" . 'mc/mark-all-like-this)
+  ("C-<" . 'mc/mark-next-like-this-symbol)
   ("C-S-<mouse-1>" . 'mc/toggle-cursor-on-click))
 
 (use-package rainbow-delimiters
